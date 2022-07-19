fa6f773827a3198fe275c1d399b0f1deed03d84e
Satinder Singh <agnosticoder@gmail.com>
2022-07-19 08:50:56 -0400

    try cors 2

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,8 +1,23 @@
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
 import { withSessionRoute } from '../../../lib/withSession';
 import createContext from '../../../server/context';
 import appRouter from '../../../server/routers/_app';
 
-export default withSessionRoute(trpcNext.createNextApiHandler({ router: appRouter, createContext}));
 
+
+const withCors = (handler:NextApiHandler) => {
+    return async (req:NextApiRequest, res:NextApiResponse) => {
+        await NextCors(req, res, {
+            // Options
+            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+            origin: '*',
+            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+         });
+         return handler(req, res);
+    }
+}
+
+export default withSessionRoute(withCors(trpcNext.createNextApiHandler({ router: appRouter, createContext})));
