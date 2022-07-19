d55a47da4507009690e45d06ff3cdd5420ea8fa4
Satinder Singh <agnosticoder@gmail.com>
2022-07-19 08:21:17 -0400

    try cors

--- server/context.ts
+++ server/context.ts
@@ -1,8 +1,16 @@
 import * as trpc from '@trpc/server';
 import { inferAsyncReturnType } from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
+import NextCors from 'nextjs-cors';
 
 const createContext = async ({req, res}:trpcNext.CreateNextContextOptions) => {
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+     });
+
     return {
         session: req.session
     }
--- utils/client.ts
+++ utils/client.ts
@@ -2,7 +2,9 @@ import type { AppRouter } from '../server/routers/_app';
 import { createTRPCClient } from '@trpc/client';
 
 const client = createTRPCClient<AppRouter>({
-    url: 'http://localhost:3000/api/trpc',   
+    url: process.env.NEXT_PUBLIC_VERCEL_URL
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+        : 'http://localhost:3000/api/trpc',
 });
 
 export default client;
