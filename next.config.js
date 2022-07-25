27ff3998ed97813babca8b6aa9c6ab5c30092ccb
Satinder Singh <agnosticoder@gmail.com>
2022-07-24 20:51:58 -0400

    vercel

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const withMDX = require('@next/mdx')();
 const url = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/:path*` : '/api/:path*';
 
-module.exports = withMDX({
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'],
+module.exports = {
     reactStrictMode: true,
     async rewrites() {
         return [
@@ -13,4 +12,4 @@ module.exports = withMDX({
           },
         ]
       },
-});
+}
