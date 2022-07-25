8679f3c0d2d399394bd9c61d0a3bdd2402bb6021
Satinder Singh <agnosticoder@gmail.com>
2022-07-24 20:58:19 -0400

    vercel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')();
-const url = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/:path*` : '/api/:path*';
+const url = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/` : '/api/:path*';
 
 module.exports = {
     reactStrictMode: true,
     async rewrites() {
         return [
           {
-            source: '/api/:path*',
+            source: '/api/',
             destination: url,
           },
         ]
