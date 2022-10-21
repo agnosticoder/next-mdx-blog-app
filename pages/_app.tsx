90b19226cf63c6e880f75ac65c76403aac87980f
Satinder Singh <agnosticoder@gmail.com>
2022-10-22 02:15:44 +0530

    Implement loading nprogress loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,34 @@ import { AppRouter } from '../server/routers/_app';
 import {ReactQueryDevtools} from 'react-query/devtools';
 import 'highlight.js/styles/atom-one-dark.css';
 import superjson from 'superjson';
+import NProgress from 'nprogress';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
 // function MyApp({ Component, pageProps }) {
+    const router = useRouter()
+
+    useEffect(() => {
+      const handleStart = (url: any) => {
+        NProgress.start()
+      }
+      const handleStop = () => {
+        NProgress.done()
+      }
+  
+      router.events.on('routeChangeStart', handleStart)
+      router.events.on('routeChangeComplete', handleStop)
+      router.events.on('routeChangeError', handleStop)
+  
+      return () => {
+        router.events.off('routeChangeStart', handleStart)
+        router.events.off('routeChangeComplete', handleStop)
+        router.events.off('routeChangeError', handleStop)
+      }
+    }, [router])
+
+
     return (
         <div>
             <Layout>
